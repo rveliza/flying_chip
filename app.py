0fb6d02394c006505c24b779363b36a8627e4f2f
Reyner Veliz <reyner@reynerveliz.com>
2025-02-20 23:21:34 -0600

    visa_route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route('/visa_usa')
+def visa_usa():
+    return render_template('visa_usa.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
