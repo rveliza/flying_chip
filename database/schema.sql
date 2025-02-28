0056e75f1597ab8b81eacd216f82989638011b0b
Reyner Veliz <reyner@reynerveliz.com>
2025-02-27 23:01:47 -0600

    Correction to schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS cheapest_flights;
 
 CREATE TABLE cheapest_flights (
-    id INTEGER PRIMARY KEY autoincrement,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     time_now TEXT NOT NULL,
     cityCodeFrom TEXT NOT NULL,
     cityCodeTo TEXT NOT NULL,
