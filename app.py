5fdaebf6b08146993957ac3e91ef51431f11ab16
Reyner Veliz <reyner@reynerveliz.com>
2025-03-12 15:57:05 -0600

    Order main page by price

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
     today = dt.now()
     today_f = today.strftime("%Y-%m-%d")
     db = get_db()
-    cur = db.execute(f'SELECT * FROM cheapest_flights WHERE time_now = "{today_f}"')
+    cur = db.execute(f'SELECT * FROM cheapest_flights WHERE time_now = "{today_f}" ORDER BY price')
     cheap_flights = cur.fetchall()
     return render_template('main.html', cheap_flights=cheap_flights, dest_images=dest_images)
 
